6b1945ec05f31c7fc1aa063676e95128dc47488e
EasonZhang12 <349734467@gapps.yrdsb.ca>
2025-11-12 14:34:11 -0500

    Create respawnCoin.java

---
+++ respawnCoin.java
@@ -0,0 +1,14 @@
+import greenfoot.*;
+    private class spawnCoin { 
+        Coin coin = new Coin();
+        int x = Greenfoot.getRandomNumber(getWidth());  
+        int y = Greenfoot.getRandomNumber(getHeight()); 
+        summon object
+    }
+    public class respawnCoin {
+        //spawn coin
+    }
+    if(coin.isTouching bee) {
+        //hide coin
+    }
+}
