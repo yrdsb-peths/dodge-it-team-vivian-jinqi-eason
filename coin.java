5b0edfe8a15c72fcb50c9c653143a793bbde7df1
EasonZhang12 <349734467@gapps.yrdsb.ca>
2025-11-12 14:21:14 -0500

    Update coin.java

--- coin.java
+++ coin.java
@@ -1,10 +1,20 @@
 import greenfoot.*;
-
-
-public class coin extends Actor
+public class Coin extends Actor
 {
     public void act()
     {
-                 
+
+    }
+}
+    private void spawnCoin()
+    {
+        Coin coin = new Coin();
+        int x = Greenfoot.getRandomNumber(getWidth());  
+        int y = Greenfoot.getRandomNumber(getHeight()); 
+        addObject(coin, x, y);
+    }
+    public void respawnCoin()
+    {
+        spawnCoin();
     }
 }
